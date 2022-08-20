6209794e7e9a9cbbe72eed9adb01b3537204548e
dafinoer <dafinoer@outlook.com>
2022-08-20 21:28:08 +0700

    create setup for push notification

--- lib/main_config.dart
+++ lib/main_config.dart
@@ -2,11 +2,13 @@ class MainConfig {
   final String title;
   final String baseUrl;
   final String apiKey;
+  final String notificationAppKey;
 
   MainConfig({
     required this.title,
     required this.baseUrl,
     required this.apiKey,
+    required this.notificationAppKey,
   });
 
   static late final MainConfig instance;
@@ -15,7 +17,9 @@ class MainConfig {
     instance = MainConfig(
         title: 'Iceman Weather',
         baseUrl: 'https://api.openweathermap.org',
-        apiKey: '<api-key>');
+        apiKey: '<api-key>',
+      notificationAppKey: '<one-signal-appKey>',
+    );
     return instance;
   }
 }
