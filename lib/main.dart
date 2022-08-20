25d993860bbc0246780890fe3882143ec27c7df3
dafinoer <dafinoer@outlook.com>
2022-08-20 21:20:52 +0700

    add widget flutter binding

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:iceman_weather/main_config.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 import 'iceman_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   MainConfig.dev();
+  final oneSignalNotification = OneSignal.shared;
+  oneSignalNotification.setAppId(MainConfig.instance.notificationAppKey);
   runApp(const IcemanApp());
 }
