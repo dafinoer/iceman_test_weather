8502d1d54f745834f072e690237e530cf6d6f462
dafinoer <dafinoer@outlook.com>
2022-08-20 21:23:19 +0700

    add notification_state.dart

---
+++ lib/presentations/notification/notification_state.dart
@@ -0,0 +1,22 @@
+part of 'notification_cubit.dart';
+
+abstract class NotificationState extends Equatable {
+  const NotificationState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class NotificationInitial extends NotificationState {
+  const NotificationInitial();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class NotificationOpenApp extends NotificationState {
+  const NotificationOpenApp();
+
+  @override
+  List<Object?> get props => [];
+}
