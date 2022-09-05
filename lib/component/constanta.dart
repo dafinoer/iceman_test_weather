6ad8f6b4e12aa5213a3c02bf98116b094a8f1c03
dafinoer <dafinoer@outlook.com>
2022-09-05 16:07:47 +0700

    create constanta.dart

---
+++ lib/component/constanta.dart
@@ -0,0 +1,8 @@
+class Constanta {
+  static const String _rootPath = 'assets/icons';
+  static const String assetPathThermometer = '$_rootPath/thermometer-half.png';
+  static const String assetCloudIcon = '$_rootPath/cloud-showers.png';
+  static const String assetTachometerIcon =
+      '$_rootPath/tachometer-fastest.png';
+  static const String degreeSymbol = '\u2103';
+}
